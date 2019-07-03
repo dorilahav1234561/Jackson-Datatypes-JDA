9ab4214020b8e33315ce2c657947b33cf29a7317
Lior Slakman <liorslakman3@gmail.com>
2019-07-03 18:06:21 +0300

    Added config options related to deserializing some types with name instead of id, to be more user friendly.

--- src/main/java/net/voigon/jackson/jda/JDAModule.java
+++ src/main/java/net/voigon/jackson/jda/JDAModule.java
@@ -1,28 +1,42 @@
 package net.voigon.jackson.jda;
 
+import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
 import com.fasterxml.jackson.databind.module.SimpleModule;
 
 import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.Accessors;
 import net.dv8tion.jda.core.JDA;
 import net.voigon.jackson.jda.deser.JDADeserializers;
 import net.voigon.jackson.jda.ser.JDASerializers;
 
+@Accessors(chain = true, fluent = true)
 public class JDAModule extends SimpleModule {
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = -1804182873632148990L;
-	
-	@Getter
+
 	private final JDA
 			bot;
-	
+
+	@Getter @Setter
+	private boolean
+			deserializeWithNames = true,
+			nameIgnoreCase = true;
+
 	public JDAModule(JDA jda) {
 		this.bot = jda;
-		
+
 		setSerializers(new JDASerializers());
 		setDeserializers(new JDADeserializers(bot));
 	}
-		
+
+	public JDA getBot() {return this.bot;}
+
+	public <T> boolean isNameIgnoreCase(StdDeserializer<T> deserializer) {
+		return nameIgnoreCase;
+	}
+
 }
