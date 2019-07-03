e55d217cbeba4b6bccd9a4f4615f650e0f5b50a6
Lior Slakman <liorslakman3@gmail.com>
2019-07-03 18:17:56 +0300

    Now requiring JDAModule to construct

--- src/main/java/net/voigon/jackson/jda/deser/JDADeserializers.java
+++ src/main/java/net/voigon/jackson/jda/deser/JDADeserializers.java
@@ -18,6 +18,7 @@ import net.dv8tion.jda.core.entities.Role;
 import net.dv8tion.jda.core.entities.TextChannel;
 import net.dv8tion.jda.core.entities.User;
 import net.dv8tion.jda.core.entities.VoiceChannel;
+import net.voigon.jackson.jda.JDAModule;
 
 public class JDADeserializers extends SimpleDeserializers {
 
@@ -26,22 +27,22 @@ public class JDADeserializers extends SimpleDeserializers {
 	 */
 	private static final long serialVersionUID = -2131720185837427613L;
 
-	public JDADeserializers(JDA bot) {
-		addDeserializer(Guild.class, new GuildDeserializer(bot));
-		addDeserializer(Member.class, new MemberDeserializer(bot));
+	public JDADeserializers(JDAModule module) {
+		addDeserializer(Guild.class, new GuildDeserializer(module));
+		addDeserializer(Member.class, new MemberDeserializer(module));
 		addDeserializer(Message.class, new MessageDeserializer());
-		addDeserializer(TextChannel.class, new TextChannelDeserializer(bot));
-		addDeserializer(User.class, new UserDeserializer(bot));
-		addDeserializer(VoiceChannel.class, new VoiceChannelDeserializer(bot));
+		addDeserializer(TextChannel.class, new TextChannelDeserializer(module));
+		addDeserializer(User.class, new UserDeserializer(module));
+		addDeserializer(VoiceChannel.class, new VoiceChannelDeserializer(module));
 		addDeserializer(AuthorInfo.class, new AuthorInfoDeserializer());
 		addDeserializer(VideoInfo.class, new VideoInfoDeserializer());
 		addDeserializer(ImageInfo.class, new ImageInfoDeserializer());
 		addDeserializer(Field.class, new FieldDeserializer());
 		addDeserializer(Footer.class, new FooterDeserializer());
-		addDeserializer(Role.class, new RoleDeserializer(bot));
-		addDeserializer(MessageEmbed.class, new MessageEmbedDeserializer(bot));
+		addDeserializer(Role.class, new RoleDeserializer(module));
+		addDeserializer(MessageEmbed.class, new MessageEmbedDeserializer(module.getBot()));
 		addDeserializer(Game.class, new GameDeserializer());
-		addDeserializer(Category.class, new CategoryDeserializer(bot));
+		addDeserializer(Category.class, new CategoryDeserializer(module));
 	}
 	
 }
